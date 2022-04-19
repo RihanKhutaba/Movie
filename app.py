1bfd4fae2e98618325276c3dbe5e05ee0f290429
RihanKhutaba <rehani.kh@gmail.com>
2022-04-19 10:45:55 +0300

    configuration phase: adding comments

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import requests
+#key: '0cc913ce4bb1b776d2e5f84ad059c224'
+#step: wide configuration
 
 CONFIG_PATTERN = 'http://api.themoviedb.org/3/configuration?api_key=0cc913ce4bb1b776d2e5f84ad059c224'
 KEY = '0cc913ce4bb1b776d2e5f84ad059c224'
@@ -6,7 +8,8 @@ KEY = '0cc913ce4bb1b776d2e5f84ad059c224'
 url = CONFIG_PATTERN.format(key=KEY)
 r = requests.get(url)
 config = r.json()
-
+""" 
+content like this: 
 {'change_keys': ['adult',
                   'also_known_as',
                   ...,
@@ -17,9 +20,13 @@ config = r.json()
              'poster_sizes': ['w92', 'w154', 'w185', 'w342', 'w500', 'original'],
              'profile_sizes': ['w45', 'w185', 'h632', 'original'],
              'secure_base_url': 'https://d3gtl9l2a4fn1j.cloudfront.net/t/p/'}}
+"""
 
+# base_url: this is where the images are stored.
+# poster_sizes: those are the available sizes.
 base_url = config['images']['base_url']
 sizes = config['images']['poster_sizes']
+
 """
     'sizes' should be sorted in ascending order, so
         max_size = sizes[-1]
